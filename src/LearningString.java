17f8b313850608238ddde61d5f5928cc13887857
celina <celina@192.168.0.9>
2020-01-18 09:43:30 -0600

    Assignment

---
+++ src/LearningString.java
@@ -0,0 +1,16 @@
+
+public class LearningString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String a = "JAVA EXERCISE";
+		String b = " DOING STRING ASSIGNMENT";
+		String c = a.substring(0,4)+ b.substring(6,24);
+		
+		System.out.println(a.substring(0,4).toLowerCase()+""+(b.substring(6,24).toLowerCase()));
+	
+	    System.out.println(c.toLowerCase());
+	}
+
+}
