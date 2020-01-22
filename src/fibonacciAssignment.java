521ca720ef1bb0a7f236d47621b02b7b8833bb6d
celina <celina@192.168.0.9>
2020-01-21 21:44:55 -0600

    something

---
+++ src/fibonacciAssignment.java
@@ -0,0 +1,20 @@
+
+public class fibonacciAssignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int firstNumber = 0;
+		int secondNumber = 1;
+		System.out.print(firstNumber + "," + secondNumber + ",");
+		
+		for (int i=1; i<=18; i++) {
+			int thirdNumber = firstNumber + secondNumber;
+			System.out.print(thirdNumber + ",");
+			firstNumber=secondNumber;
+			secondNumber=thirdNumber;
+		}
+	}
+	
+
+}
