a6cf272cf6215826cd4b661a911cb9b969ff5eca
celina <celina@192.168.0.9>
2020-01-22 19:07:25 -0600

    new assignment

--- src/LoopAssignment.java
+++ src/LoopAssignment.java
@@ -3,21 +3,42 @@ public class LoopAssignment {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 
 
-		for(int i=1;i<=5;i++){
-			for(int j=1;j<=i;j++){
-			System.out.print("* ");
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= i; j++) {
+				if ((j == 1) || (j == i)) {
+					System.out.print("* ");
+				} 
+//				 if(i==5)
+//				{
+//					System.out.print("* ");
+//
+//				}
+				else {
+					System.out.print(" ");
+				}
+
 			}
-			System.out.println();//new line
-			}	
-		
-	int sum=0;
-	for(int i=1; i<=100; i++) {
-		sum= i+sum;
-		
-	}
-	System.out.println("Total sum:"+sum);
+			System.out.println();// new line
+		}
+
+		int sum = 0;
+		for (int i = 1; i <= 100; i++) {
+			sum = i + sum;
+
+		}
+		System.out.println("Total sum:" + sum);
 	
+	for(int i=1; i<=5; i++) {
+		
+		System.out.println();
+		
+		for( int j=1; j<=i; j++) {
+			System.out.print("x");
+			
+		}
 	}
+   }
 }
+
+	
---
+++ src/ReverseString.java
@@ -0,0 +1,18 @@
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+ String color = "GREEN";
+ int reverse = color.length();
+ 
+ for (int i = reverse-1; i>= 0; i-- ) {
+	 System.out.print(color.charAt(i));
+
+ }
+	}
+ 
+}
+
